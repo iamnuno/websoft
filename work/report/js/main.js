337e0c0c95c317931a1a7df5fb6ac1f519cd8f78
Nuno Cunha <33864862+iamnuno@users.noreply.github.com>
2020-01-27 23:30:48 +0100

    Added initial moveable duck

--- work/report/js/main.js
+++ work/report/js/main.js
@@ -4,7 +4,22 @@
 (function () {
     "use strict";
 
+    console.log("Duck start...");
+    var element = document.getElementById("duck");
 
-
-    console.log("All ready.");
+    element.addEventListener("click", function () {
+        console.log("Duck clicked.");
+        var left = getPosition(element).left;
+        if (left + element.width + (element.width / 2) < window.innerWidth) {
+          element.style.left = element.offsetLeft + 150 +'px';
+        }
+    });
 }());
+
+function getPosition(element) {
+  const rect = element.getBoundingClientRect();
+  return {
+    left: rect.left + window.scrollX,
+    top: rect.top + window.scrollY
+  };
+}
