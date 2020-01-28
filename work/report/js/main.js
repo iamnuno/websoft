a67af30e2f4f24ff4f81d6a5803fe29db209cbb8
iamnuno <33864862+iamnuno@users.noreply.github.com>
2020-01-28 14:11:11 +0100

    Refactored main.js

--- work/report/js/main.js
+++ work/report/js/main.js
@@ -4,22 +4,26 @@
 (function () {
     "use strict";
 
-    console.log("Duck start...");
+    duck();
 
-    var body = document.getElementById("body");
-    var duck = document.getElementById("duck");
-
-    duck.addEventListener("click", function () {
-        console.log("Duck clicked.");
-        var duck_xPos = get_xPosition(duck);
-        var duck_width = duck.offsetWidth;
-        var body_width = body.offsetWidth;
-        var moveRightPx = 15;
-        if (duck_xPos + duck_width + moveRightPx < body_width) {
-          duck.style.left = duck.offsetLeft + moveRightPx +'px';
-        }
-    });
 }());
+
+function duck() {
+  console.log("Duck start...");
+
+  var body = document.getElementById("body");
+  var duck = document.getElementById("duck");
+
+  duck.addEventListener("click", function () {
+      var duck_xPos = get_xPosition(duck);
+      var duck_width = duck.offsetWidth;
+      var body_width = body.offsetWidth;
+      var moveRightPx = 15;
+      if (duck_xPos + duck_width + moveRightPx < body_width) {
+        duck.style.left = duck.offsetLeft + moveRightPx +'px';
+      }
+  });
+}
 
 function get_xPosition(element) {
   const rect = element.getBoundingClientRect();
