d4471a8ced1e1a01d519d57042f0dd27929aa5a6
iamnuno <33864862+iamnuno@users.noreply.github.com>
2020-01-28 15:20:55 +0100

    Duck visibility changes every 5 secs

--- work/report/js/main.js
+++ work/report/js/main.js
@@ -1,11 +1,10 @@
 /**
- * A function to wrap it all in.
- */
+* A function to wrap it all in.
+*/
 (function () {
-    "use strict";
+  "use strict";
 
-    duck();
-
+  duck();
 }());
 
 function duck() {
@@ -14,15 +13,27 @@ function duck() {
   var body = document.getElementById("body");
   var duck = document.getElementById("duck");
 
+  toggleVisibilityTimer(duck);
+
   duck.addEventListener("click", function () {
-      var duck_xPos = get_xPosition(duck);
-      var duck_width = duck.offsetWidth;
-      var body_width = body.offsetWidth;
-      var moveRightPx = 15;
-      if (duck_xPos + duck_width + moveRightPx < body_width) {
-        duck.style.left = duck.offsetLeft + moveRightPx +'px';
-      }
+    var duck_xPos = get_xPosition(duck);
+    var duck_width = duck.offsetWidth;
+    var body_width = body.offsetWidth;
+    var moveRightPx = 15;
+    if (duck_xPos + duck_width + moveRightPx < body_width) {
+      duck.style.left = duck.offsetLeft + moveRightPx +'px';
+    }
   });
+}
+
+function toggleVisibilityTimer(element) {
+  setInterval (function () {
+    if (element.style.visibility === 'hidden') {
+      element.style.visibility = 'visible';
+    } else {
+      element.style.visibility = 'hidden';
+    }
+  }, 5000);
 }
 
 function get_xPosition(element) {
