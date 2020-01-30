6381525185bc60d0d8b7ec115e87c9d8f472c2cf
Nuno Cunha <33864862+iamnuno@users.noreply.github.com>
2020-01-30 15:25:42 +0100

    Added S03 text to report.html and some refactoring

--- work/report/js/main.js
+++ work/report/js/main.js
@@ -11,14 +11,14 @@
 }());
 
 function duck() {
-  console.log("Duck start...");
+  console.log('Duck start...');
 
-  var body = document.getElementById("body");
-  var duck = document.getElementById("duck");
+  var body = document.getElementById('body');
+  var duck = document.getElementById('duck');
 
   toggleVisibilityTimer(duck);
 
-  duck.addEventListener("click", function () {
+  duck.addEventListener('click', function () {
     var duck_xPos = get_xPosition(duck);
     var duck_width = duck.offsetWidth;
     var body_width = body.offsetWidth;
@@ -45,53 +45,53 @@ function get_xPosition(element) {
 }
 
 function flags() {
-  console.log("Flags started...");
+  console.log('Flags started...');
 
-  var france = document.getElementById("france_link");
-  var italy = document.getElementById("italy_link");
-  var belgium = document.getElementById("belgium_link");
+  var france = document.getElementById('france_link');
+  var italy = document.getElementById('italy_link');
+  var belgium = document.getElementById('belgium_link');
 
-  var france_flag = document.getElementById("france_flag");
-  var italy_flag = document.getElementById("italy_flag");
-  var belgium_flag = document.getElementById("belgium_flag");
+  var france_flag = document.getElementById('france_flag');
+  var italy_flag = document.getElementById('italy_flag');
+  var belgium_flag = document.getElementById('belgium_flag');
 
   //show the flag when clicking the link
   if (france !== null) {
-    france.addEventListener("click", function () {
-      france_flag.style.opacity = "1";
+    france.addEventListener('click', function () {
+      france_flag.style.opacity = '1';
     });
   }
   if (italy !== null) {
-    italy.addEventListener("click", function () {
-      italy_flag.style.opacity = "1";
+    italy.addEventListener('click', function () {
+      italy_flag.style.opacity = '1';
     });
   }
   if (belgium !== null) {
-    belgium.addEventListener("click", function () {
-      belgium_flag.style.opacity = "1";
+    belgium.addEventListener('click', function () {
+      belgium_flag.style.opacity = '1';
     });
   }
 
   //hide the flag when clicking the flag
   if (france_flag !== null) {
-    france_flag.addEventListener("click", function () {
-      france_flag.style.opacity = "0";
+    france_flag.addEventListener('click', function () {
+      france_flag.style.opacity = '0';
     });
   }
   if (italy_flag !== null) {
-    italy_flag.addEventListener("click", function () {
-      italy_flag.style.opacity = "0";
+    italy_flag.addEventListener('click', function () {
+      italy_flag.style.opacity = '0';
     });
   }
   if (belgium_flag !== null) {
-    belgium_flag.addEventListener("click", function () {
-      belgium_flag.style.opacity = "0";
+    belgium_flag.addEventListener('click', function () {
+      belgium_flag.style.opacity = '0';
     });
   }
 }
 
 function municipalities() {
-  console.log("Fetch kommuner.");
+  console.log('Fetch kommuner.');
   fetch('data/kommun.json')
   .then((response) => {
     return response.json();
@@ -105,9 +105,9 @@ function municipalities() {
     document.getElementById('kommuner').innerHTML = select;
   });
 
-  var selectKommuner = document.getElementById("kommuner");
+  var selectKommuner = document.getElementById('kommuner');
   selectKommuner.addEventListener('change', (event) => {
-    
+
     if (event.target.value == 'default') {
       document.getElementById('table').innerHTML = '';
     } else {
@@ -118,14 +118,14 @@ function municipalities() {
 }
 
 function fecthSchools(kommuner) {
-  var kommunCode = "";
+  var kommunCode = '';
 
-  if (kommuner === "Kristianstad") {
-    kommunCode = "1290";
-  } else if (kommuner === "Malmö") {
-    kommunCode = "1280";
-  } else if (kommuner === "Stockholm") {
-    kommunCode = "0180";
+  if (kommuner === 'Kristianstad') {
+    kommunCode = '1290';
+  } else if (kommuner === 'Malmö') {
+    kommunCode = '1280';
+  } else if (kommuner === 'Stockholm') {
+    kommunCode = '0180';
   }
 
   fetch('data/' + kommunCode + '.json')
